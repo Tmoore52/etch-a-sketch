0f4ddaaea50f18834132119f1afc7225ff2fde25
Tyler Moore <tylermoore5252@gmail.com>
2022-10-18 20:26:37 -0400

    Fixed random color to grid items

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # etch-a-sketch
+
+Project using the Javascript skills I've learned so far.
+
+Was familiar with DOM, but this one made me dig a little deeper than I have before.
--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 let grid = document.querySelector("#grid-container");
+// creates grid based on col and row multiplied by each other
 
 function createGrid(col, row) {
-    col = col * row;
-    for(i = 0; i < col; i++){
-         let gridItem = document.createElement('div');
-         grid.appendChild(gridItem);
-         gridItem.className = 'grid-item';
-        //  gridItem.innerText = i + 1;
-    }
+  col = col * row;
+  for (i = 0; i < col; i++) {
+    gridItem = document.createElement("div");
+    grid.appendChild(gridItem).className = 'grid-item';
+    gridItem.addEventListener("mouseenter", colorChange);
+  }
 }
 
-createGrid(16, 16);
+function randomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+createGrid(16,16);
+
+function colorChange(e) {
+  console.log("EVENT TYPE:" + e.type);
+  e.target.style.backgroundColor = randomColor();
+}
