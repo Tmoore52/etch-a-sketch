08bd1d53c26624c7a55096b543d2a72397760ab8
Tyler Moore <tylermoore5252@gmail.com>
2022-10-18 20:55:19 -0400

    Button working need to fix input
    Not setting the prompts input to the variable col. Need to debug and find
    out why.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let grid = document.querySelector("#grid-container");
+let button = document.querySelector(".button");
+col = 16;
+row = col;
 // creates grid based on col and row multiplied by each other
 
 function createGrid(col, row) {
   col = col * row;
   for (i = 0; i < col; i++) {
     gridItem = document.createElement("div");
-    grid.appendChild(gridItem).className = 'grid-item';
+    grid.appendChild(gridItem).className = `grid-item ${i + 1}`;
     gridItem.addEventListener("mouseenter", colorChange);
   }
+  button.addEventListener("click", squareChange);
 }
 
 function randomColor() {
@@ -19,9 +23,20 @@ function randomColor() {
   return color;
 }
 
-createGrid(16,16);
+createGrid(col, row);
 
 function colorChange(e) {
   console.log("EVENT TYPE:" + e.type);
   e.target.style.backgroundColor = randomColor();
 }
+
+function squareChange() {
+  let col = prompt("Enter how squares per side you'd like up to 100");
+  if (col > 100 || col < 1) {
+    alert("Number too high or low. Please try again and enter a number between 1 and 100.");
+  }
+  row = col;
+  console.log(col);
+  console.log(row);
+};
+
